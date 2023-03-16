febc5178404a94304d7da3486b25fb3f96524b33
Sodiq-Abdulsalam <sodiqabdulsalam@gmail.com>
2023-03-16 13:42:04 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is the entry or start point of the code
+ *
+ * Return:0 when the main function is successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
