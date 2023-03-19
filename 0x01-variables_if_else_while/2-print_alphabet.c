7e5fb8e3fdc51fb223dc1374901d84b13a41ae90
Sodiq-Abdulsalam <sodiqabdulsalam@gmail.com>
2023-03-19 12:43:30 -0700

    print a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - start piont of code block
+ *
+ * Return: return 0 when code is successful
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+}
