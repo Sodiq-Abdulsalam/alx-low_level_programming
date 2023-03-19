93b7782bf651b7299cb03152a8aea2f9de7d51d6
Sodiq-Abdulsalam <sodiqabdulsalam@gmail.com>
2023-03-19 14:09:43 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - start point of the code
+ *
+ * Return: return 0 whenr code is successfule
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+
+}
