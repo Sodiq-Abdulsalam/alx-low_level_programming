2b7017973315475d806765bb3e0f96760a0dc26b
Sodiq-Abdulsalam <sodiqabdulsalam@gmail.com>
2023-03-19 15:49:22 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - start point for code block
+ *
+ * return: return 0 when code is successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
