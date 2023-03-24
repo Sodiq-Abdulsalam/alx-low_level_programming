46dc5239da10060932303350750b08fdca389a72
Sodiq-Abdulsalam <sodiqabdulsalam@gmail.com>
2023-03-24 14:24:16 -0700

    task 7

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,19 @@
  */
 void print_line(int n)
 {
-	int i;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i;
 
-	if (n <= 0)
-	_putchar('\n');
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
 
-	else
-	{
-		for (i = 0; i <= n; i++)
-		_putchar('_');
-		_putchar('\n');
-	}
 }
