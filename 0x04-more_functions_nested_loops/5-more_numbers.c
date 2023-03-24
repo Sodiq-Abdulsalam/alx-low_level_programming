a7a5fc3f6d87e4876ac6a1bce10d1132d1cc5826
Sodiq-Abdulsalam <sodiqabdulsalam@gmail.com>
2023-03-24 12:47:14 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that print 0 - 14 x10 times
+ *
+ * Return: 0-14 x10
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 0 ; j <= 14 ; j++)
+			{
+				if (j > 9)
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
+
+		_putchar('\n');
+	}
+}
